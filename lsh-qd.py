d986a49dbd4c4907fad4768de275b1ab74534556
Sam Earle <smearle93@gmail.com>
2021-12-17 18:17:32 -0500

    Plot theoretical collision probabilities w/ alpha.
    There seems to be a discrepancy between our calculation for vanilla LSH
    and when alpha=1, which should reduce to vanilla. Investigating this
    next.

--- lsh-qd.py
+++ lsh-qd.py
@@ -1,8 +1,11 @@
+import math
 import numpy as np
 from tqdm import tqdm
 from collections import defaultdict
-from scipy.spatial.distance import jaccard
+from matplotlib import pyplot as plt
+from scipy.spatial.distance import jaccard  # jaccard distance measure
 
+figs_dir = 'figs'
 
 class AlphaMinhash():
     '''
@@ -129,13 +132,17 @@ class AlphaLSHContainer():
         self.data = defaultdict(int)
 
     def store(self, input_data):
-        '''Store input data in the LSH scheme.'''
+        '''
+        Store a batch of input data in the LSH scheme. The LSH scheme only stores data indices, which this container
+        stores the actual datapoints.
+        Args:
+            input_data: a (n_data, n_dims)-shape array of new datapoints to be stored in the container
+        '''
         input_data_idxs = []
         for row_idx, data_point in tqdm(enumerate(input_data), desc="Hashing data", total=input_data.shape[0]):
             data_idx, bucket_ids = self.lsh.minhash(data_point)
-            print(f'Stored data point {data_idx}')
             assert data_idx not in self.data
-            self.data[data_idx] = input_data
+            self.data[data_idx] = data_point
             input_data_idxs.append(data_idx)
         return input_data_idxs
 
@@ -146,24 +153,115 @@ class AlphaLSHContainer():
         return self.lsh.query_idx(x, alpha=alpha, threshold=threshold)
 
 
-if __name__ == "__main__":
+def collision_prob(sim, r, t):
+    '''
+    Compute the collisiion probabilities between pairs of items with given similarity scores (or inverse distances)
+    between them.
+    Args:
+        sim: an integer or 1D vector of similarities between in [0, 1]
+        r (int): the number of bands in each LSH instance in the hashing scheme
+        t (int): the number of tables (LSH instances) in the hashing scheme
+    Returns:
+        an integer or 1D vector of collision probabilities in [0, 1]
+    '''
+    p_coll = 1 - (1 - sim ** r) ** t
+    return p_coll
+
+def collision_prob_alpha(sim, r, t, alpha=1):
+    '''
+    Compute the collision probabities given similarity scores for a given value of alpha in the alpha-tunable
+    LSH scheme.
+    Args:
+        sim: an integer or 1D vector of similarities between in [0, 1]
+        r (int): the number of bands in each LSH instance in the hashing scheme
+        t (int): the number of tables (LSH instances) in the hashing scheme
+    Returns:
+        an integer or 1D vector of collision probabilities in [0, 1]
+    '''
+    p_coll = 0
+    for i in range(int(alpha), t + 1):
+        p_coll += math.comb(t, i) * (sim ** r) ** i * (1 - sim ** r) ** (t - i)
+    if alpha == 1:
+        # ensure we achieve the same s-curve as the equivalent vanilar LSH scheme when alpha=1
+        vanilla_p_coll = collision_prob(sim, r, t)
+        # FIXME: there seems to be a small discrepancy here?
+        assert np.all(p_coll == vanilla_p_coll)
+    return p_coll
+
+def plot_collision_prob_alpha(r, t):
+    v = np.arange(0, 1.01, 1 / 100)  # the
+    alphas = np.arange(0, t + 1, t / 20)
+    for a in alphas:
+        p_coll = collision_prob_alpha(v, r, t, alpha=a)
+        label = f'alpha = {int(a)}' if a == alphas[0] or a == alphas[-1] else None
+        plt.plot(v, p_coll, label=label, color='blue', alpha=0.2 + 0.8 * (a / (alphas[-1] - alphas[0])))
+    plt.xlabel('item similarity')
+    plt.ylabel('collision probability')
+    plt.legend()
+    plt.savefig(f'{figs_dir}/coll_prob_alpha_r-{r}_t-{t}.png')
+    plt.close()
+
+def plot_mean_nbs_by_alpha():
+    '''
+    Plot the effect of the LSH alpha parameter on the average neighborhood size and intra-neighborhood similarity in
+     neighborhoods returned by the LSH scheme.
+    '''
     r = 1
-    t = 5
-    lsh_container = AlphaLSHContainer(t=t, r=1)
+    t = 12
+    lsh_container = AlphaLSHContainer(t=t, r=r)
     dims = 1000
     n_data = 100
     data = np.random.randint(0, 2, (n_data, dims))
     new_idxs = lsh_container.store(data)
     assert new_idxs == list(range(n_data))
-    q_idx = 42
+    n_queries = 10
+    q_idxs = np.random.choice(list(lsh_container.data.keys()), n_queries, replace=False)
     alphas = range(1, t)
+    # TODO: Is this equivalent to just averaging over *all* alpha-neighborhoods in our container? Is there an easy way
+    #   to collect all these neighborhoods?
+    a_n_nbs = []  # mean neighborhood size per alpha
+    a_sims = []  # mean similarity within a neighborhood per alpha
     for alpha in alphas:
-        nb_idxs = lsh_container.query_idx(q_idx)
-        print(nb_idxs)
-        print([1 - jaccard(data[q_idx], data[nb_idx]) for nb_idx in nb_idxs])
+        q_n_nbs = []  # neighborhood size for each query item
+        q_sims = []  # mean intra-neighborhood similarity for each query item
+        for q_idx in q_idxs:
+            nb_idxs = lsh_container.query_idx(q_idx, alpha=alpha)
+            n_nbs = len(nb_idxs)
+            mean_sim = np.mean([1 - jaccard(lsh_container.data[i], lsh_container.data[j]) for i in nb_idxs
+                               for j in nb_idxs if i > j])
+            q_n_nbs.append(n_nbs)
+            q_sims.append(mean_sim)
+            # print(nb_idxs)
+            # print([1 - jaccard(lsh_container.data[q_idx], lsh_container.data[nb_idx]) for nb_idx in nb_idxs])
+        a_n_nbs.append(np.mean(q_n_nbs))
+        a_sims.append(np.mean(q_sims))
+
+    plt.plot(alphas, a_n_nbs)
+    plt.ylabel('mean number of neighbors')
+    plt.xlabel('alpha')
+    plt.savefig(f'{figs_dir}/mean_nbs_X_alpha')
+    plt.close()
+
+    # a_sims = np.where(np.isnan(a_sims), 1, a_sims)
+    plt.plot(alphas, a_sims)
+    plt.ylabel('mean intra-neighborhood similarity')
+    plt.xlabel('alpha')
+    plt.tight_layout()
+    plt.savefig(f'{figs_dir}/mean_sim_X_alpha')
+    plt.close()
+
     # test_idx = 42
     # bucket_ids = lsh.data_idx_to_bucket_ids[test_idx]
     # collision_idxs = list(set(sum([lsh.tables[hash_idx][bucket_id] for hash_idx, bucket_id in enumerate(bucket_ids)], [])))
     # collision_idxs.remove(test_idx)
     # print(f"Test index {test_idx} collides with {collision_idxs}")
     # print("Average Jaccard similarity of collisions:", np.mean([jaccard(data[test_idx], data[collision_idx]) for collision_idx in collision_idxs]))
+
+
+if __name__ == "__main__":
+    plot_mean_nbs_by_alpha()
+    rs = range(1, 10)
+    ts = range(1, 20)
+    for r in rs:
+        for t in ts:
+            plot_collision_prob_alpha(r, t)
