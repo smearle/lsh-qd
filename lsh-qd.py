62c32d52766ef33869adde29deb457e10255b08c
Sam Earle <smearle93@gmail.com>
2021-12-17 16:26:33 -0500

    Implemented data structure for storing datapoints.
    It uses the existing LSH scheme to find near neighbors.

--- lsh-qd.py
+++ lsh-qd.py
@@ -24,7 +24,7 @@ class AlphaMinhash():
                  r,
                  seed=42):
         self.t, self.r = t, r
-        self.seed = 42
+        self.seed = seed
         # We use the largest 32-bit integer as our maximum hash value, but
         # this could be changed without too much effort
         self.max_val = 2 ** 32 - 1
@@ -78,11 +78,28 @@ class AlphaMinhash():
             all_bucket_ids.append(bucket_id)
         # Associate the bucket ids with the current data point
         self.data_idx_to_bucket_ids[self.cur_data_idx] = all_bucket_ids
+        data_idx = self.cur_data_idx
         # Increment the data index
         self.cur_data_idx += 1
-        return all_bucket_ids
+        return data_idx, all_bucket_ids
 
-    def query(self, query_idx, alpha=1, threshold=0):
+    def query(self, x, alpha=1, threshold=0):
+        nonzero_indexes = np.where(x == 1)[0]
+        if len(nonzero_indexes) == 0:
+            return
+        collision_freqs = defaultdict(int)
+        for hash_idx, bands in enumerate(self.hash_functions):
+            signature = [np.min(function(nonzero_indexes)) for function in bands]
+            bucket_id = hash(tuple(signature))
+            for data_index in self.tables[hash_idx][bucket_id]:
+                collision_freqs[data_index] += 1
+        filtered_collisions = [idx for idx, freq in collision_freqs.items() if freq >= alpha]
+        if threshold > 0:
+            # filtered_collisions = [idx for idx in filtered_collisions if jaccard()]
+            pass
+        return filtered_collisions
+
+    def query_idx(self, query_idx, alpha=1, threshold=0):
         '''
         Returns the indices of all hashed data points that collide with the item
         in at least alpha of the t tables. If specified, additionally returns
@@ -105,20 +122,45 @@ class AlphaMinhash():
         return filtered_collisions
 
 
-if __name__ == "__main__":
-    lsh = AlphaMinhash(t=5, r=1)
-    data = np.random.randint(0, 2, (100, 1000))
-    for data_idx, data_point in tqdm(enumerate(data), desc="Hashing data", total=data.shape[0]):
-        lsh.minhash(data_point)
-    print(lsh.query(42, 1))
-    print([jaccard(data[42], data[idx]) for idx in lsh.query(42, 1)])
+class AlphaLSHContainer():
+    '''A data structure that uses alpha-tunable LSH to compute approximate nearest neighbors.'''
+    def __init__(self, t=5, r=1, seed=42):
+        self.lsh = AlphaMinhash(t=t, r=r, seed=seed)
+        self.data = defaultdict(int)
 
-    print(lsh.query(42, 2))
-    print([jaccard(data[42], data[idx]) for idx in lsh.query(42, 2)])
-    print(lsh.query(42, 3))
-    print([jaccard(data[42], data[idx]) for idx in lsh.query(42, 3)])
-    print(lsh.query(42, 4))
-    print([jaccard(data[42], data[idx]) for idx in lsh.query(42, 4)])
+    def store(self, input_data):
+        '''Store input data in the LSH scheme.'''
+        input_data_idxs = []
+        for row_idx, data_point in tqdm(enumerate(input_data), desc="Hashing data", total=input_data.shape[0]):
+            data_idx, bucket_ids = self.lsh.minhash(data_point)
+            print(f'Stored data point {data_idx}')
+            assert data_idx not in self.data
+            self.data[data_idx] = input_data
+            input_data_idxs.append(data_idx)
+        return input_data_idxs
+
+    def query_idx(self, data_idx, alpha=1, threshold=0):
+        return self.lsh.query_idx(data_idx, alpha=alpha, threshold=threshold)
+
+    def query(self, x, alpha=1, threshold=0):
+        return self.lsh.query_idx(x, alpha=alpha, threshold=threshold)
+
+
+if __name__ == "__main__":
+    r = 1
+    t = 5
+    lsh_container = AlphaLSHContainer(t=t, r=1)
+    dims = 1000
+    n_data = 100
+    data = np.random.randint(0, 2, (n_data, dims))
+    new_idxs = lsh_container.store(data)
+    assert new_idxs == list(range(n_data))
+    q_idx = 42
+    alphas = range(1, t)
+    for alpha in alphas:
+        nb_idxs = lsh_container.query_idx(q_idx)
+        print(nb_idxs)
+        print([1 - jaccard(data[q_idx], data[nb_idx]) for nb_idx in nb_idxs])
     # test_idx = 42
     # bucket_ids = lsh.data_idx_to_bucket_ids[test_idx]
     # collision_idxs = list(set(sum([lsh.tables[hash_idx][bucket_id] for hash_idx, bucket_id in enumerate(bucket_ids)], [])))
