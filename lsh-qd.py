369b2027e04d836635aed7ac4662daf5f12b9916
Sam Earle <smearle93@gmail.com>
2021-12-18 17:39:43 -0500

    Create vanilla MinHash base class.
    Refactor some query code into "get_collision_freqs" methods. Expand on
    difference between query_idx and query in docstrings.

--- lsh-qd.py
+++ lsh-qd.py
@@ -12,39 +12,33 @@ figs_dir = 'figs'
 coll_probs_dir = os.path.join(figs_dir, 'coll_probs')
 
 
-class AlphaMinhash():
+class MinHash():
     '''
-    An implementation of the Minhash algorithm for approximate nearest
-    neighbors on binary strings, designed for use in niching and QD. In
-    addition to the typical parameters of # of tables and # of bands, this
-    also accepts an alpha parameter when querying. An item in the database
-    is returned only if it is hashed into the same bucket as the query in
-    at least alpha of the t tables.
+    An implementation of the MinHash algorithm for approximate nearest neighbor search over binary strings.
     Args:
-        t: the number of distinct hash tables
-        r: the number of bands (i.e. hash functions) used to determine an item's
-            location in each of the t tables
-        seed: the random seed for generating the hash functions
+        t: the number of hash tables
+        r: the number of bands
     '''
 
-    def __init__(self,
-                 t,
-                 r,
-                 seed=42):
+    def __init__(self, t, r, seed=42):
         self.t, self.r = t, r
         self.seed = seed
+
         # We use the largest 32-bit integer as our maximum hash value, but
         # this could be changed without too much effort
         self.max_val = 2 ** 32 - 1
         self.p = 4294967311
+
         # Store each of hash function in an array of size (t, r). self.hash_functions[i][j]
         # indicates the j'th band of the i'th table
         self.hash_functions = [[self._generate_hash_function() for _ in range(self.r)]
                                for _ in range(self.t)]
+
         # This dictionary maps from a data index to a list of t bucket ids
         # indicating the keys in each hash table where the index can be found
         self.cur_data_idx = 0
         self.data_idx_to_bucket_ids = {}
+
         # This list stores t dictionaries, each of which maps from a hash value /
         # bucket id to each of the data indexes which have been hashed to that bucket
         self.tables = [defaultdict(list) for _ in range(self.t)]
@@ -74,7 +68,9 @@ class AlphaMinhash():
         nonzero_indexes = np.where(x == 1)[0]
         if len(nonzero_indexes) == 0:
             return
+
         all_bucket_ids = []
+
         # For each set of r bands...
         for hash_idx, bands in enumerate(self.hash_functions):
             # Compute the data point's signature under those hash functions, and its
@@ -84,30 +80,133 @@ class AlphaMinhash():
             # Add the data index to the current bucket
             self.tables[hash_idx][bucket_id].append(self.cur_data_idx)
             all_bucket_ids.append(bucket_id)
+
         # Associate the bucket ids with the current data point
         self.data_idx_to_bucket_ids[self.cur_data_idx] = all_bucket_ids
         data_idx = self.cur_data_idx
+
         # Increment the data index
         self.cur_data_idx += 1
+
         return data_idx, all_bucket_ids
 
-    def query(self, x, alpha=1, threshold=0):
+    def query(self, x, threshold=0):
+        '''
+        Find the approximate near-neighbors of a given query item x.
+        Args:
+            x: the query item, a binary string
+        Returns:
+            nb_idxs (list(int)): a list of indices of items with which the query item collides ("neighbors")
+        '''
+        collision_freqs = self._get_collision_freqs(x)
+        nb_idxs = list(collision_freqs.keys())
+
+        return nb_idxs
+
+    def query_idx(self, query_idx, alpha=1):
+        '''
+        Like query(), but assumes the item has already been hashed.Returns the indices of all hashed data points that
+        collide with the item in at least alpha of the t tables.
+
+        *DEPRECATED* TODO: integrate this incontainer class.
+        If specified, additionally returns
+        only those items with a Jaccard similarity above a threshold
+        Args:
+            query_idx: the data index of the item to query on
+            alpha: the number of collisions in order to return a match
+            threshold: the minimum Jaccard similarity in order to return a match
+        Returns:
+            nb_idxs (:obj: `list` of :obj: `int`):
+        '''
+        collision_freqs = self._get_collision_freqs_idx(query_idx)
+        nb_idxs = list(collision_freqs.keys())
+
+        return nb_idxs
+
+    def _get_collision_freqs(self, x):
+        '''
+        Args:
+            query_idx:
+        Returns:
+            collision_freqs (dict{int: int})): a dictionary mapping the index of each neighbor to the number of buckets
+                in which it collided with the query item (always less than or equal to t, the # of total tables)
+        '''
         nonzero_indexes = np.where(x == 1)[0]
+
         if len(nonzero_indexes) == 0:
             return
+
         collision_freqs = defaultdict(int)
+
         for hash_idx, bands in enumerate(self.hash_functions):
+
+            # "AND" operation over r bands to get the low-dimensional projection of each item
             signature = [np.min(function(nonzero_indexes)) for function in bands]
+
+            # Hash the resulting signature into each of t tables
             bucket_id = hash(tuple(signature))
+
+            # Keep a count of number-of-collisions with each item
             for data_index in self.tables[hash_idx][bucket_id]:
                 collision_freqs[data_index] += 1
-        filtered_collisions = [idx for idx, freq in collision_freqs.items() if freq >= alpha]
-        if threshold > 0:
-            # filtered_collisions = [idx for idx in filtered_collisions if jaccard()]
-            pass
-        return filtered_collisions
 
-    def query_idx(self, query_idx, alpha=1, threshold=0):
+        return collision_freqs
+
+
+    def _get_collision_freqs_idx(self, query_idx):
+        '''
+        Gets the collision frequencies of an item with items already hashed to the scheme. Assumes the query item has
+        already been hashed.
+        Returns:
+            collision_freqs (dict(int: int)): a dictionary mapping the index of each neighbor to the number of buckets
+                in which it collided with the query item (always less than or equal to t, the # of total tables)
+        '''
+        bucket_ids = self.data_idx_to_bucket_ids[query_idx]
+        collision_freqs = defaultdict(int)
+
+        for hash_idx, bucket_id in enumerate(bucket_ids):
+            for data_index in self.tables[hash_idx][bucket_id]:
+                if data_index != query_idx:
+                    collision_freqs[data_index] += 1
+
+        return collision_freqs
+
+
+class AlphaMinHash(MinHash):
+    '''
+    An implementation of the MinHash algorithm for approximate nearest
+    neighbors on binary strings, designed for use in niching and QD. In
+    addition to the typical parameters of # of tables and # of bands, this
+    also accepts an alpha parameter when querying. An item in the database
+    is returned only if it is hashed into the same bucket as the query in
+    at least alpha of the t tables.
+    Args:
+        t: the number of distinct hash tableks
+        r: the number of bands (i.e. hash functions) used to determine an item's
+            location in each of the t tables
+        seed: the random seed for generating the hash functions
+    '''
+
+    def __init__(self,
+                 t,
+                 r,
+                 seed=42):
+        super(AlphaMinHash, self).__init__(t=t, r=r, seed=seed)
+
+    def query(self, x, alpha=1):
+        '''
+        Args:
+            alpha (int): minimum number of tables in which an item must collide with x in order t be considered a
+                neighbor
+        Returns:
+            nb_idxs (list(int)): a list of indices of neighbor items
+        '''
+        collision_freqs = super()._get_collision_freqs(x)
+        nb_idxs = [idx for idx, freq in collision_freqs.items() if freq >= alpha]
+
+        return nb_idxs
+
+    def query_idx(self, query_idx, alpha=1):
         '''
         Returns the indices of all hashed data points that collide with the item
         in at least alpha of the t tables. If specified, additionally returns
@@ -117,23 +216,16 @@ class AlphaMinhash():
             alpha: the number of collisions in order to return a match
             threshold: the minimum Jaccard similarity in order to return a match
         '''
-        bucket_ids = self.data_idx_to_bucket_ids[query_idx]
-        collision_freqs = defaultdict(int)
-        for hash_idx, bucket_id in enumerate(bucket_ids):
-            for data_index in self.tables[hash_idx][bucket_id]:
-                if data_index != query_idx:
-                    collision_freqs[data_index] += 1
+        collision_freqs = super()._get_collision_freqs_idx(query_idx)
         filtered_collisions = [idx for idx, freq in collision_freqs.items() if freq >= alpha]
-        if threshold > 0:
-            # filtered_collisions = [idx for idx in filtered_collisions if jaccard()]
-            pass
+
         return filtered_collisions
 
 
 class AlphaLSHContainer():
     '''A data structure that uses alpha-tunable LSH to compute approximate nearest neighbors.'''
     def __init__(self, t=5, r=1, seed=42):
-        self.lsh = AlphaMinhash(t=t, r=r, seed=seed)
+        self.lsh = AlphaMinHash(t=t, r=r, seed=seed)
         self.data = defaultdict(int)
 
     def store(self, input_data):
@@ -149,13 +241,14 @@ class AlphaLSHContainer():
             assert data_idx not in self.data
             self.data[data_idx] = data_point
             input_data_idxs.append(data_idx)
+
         return input_data_idxs
 
     def query_idx(self, data_idx, alpha=1, threshold=0):
-        return self.lsh.query_idx(data_idx, alpha=alpha, threshold=threshold)
+        return self.lsh.query_idx(data_idx, alpha=alpha)
 
     def query(self, x, alpha=1, threshold=0):
-        return self.lsh.query_idx(x, alpha=alpha, threshold=threshold)
+        return self.lsh.query_idx(x, alpha=alpha)
 
 
 def collision_prob(sim, r, t):
@@ -170,6 +263,7 @@ def collision_prob(sim, r, t):
         an integer or 1D vector of collision probabilities in [0, 1]
     '''
     p_coll = 1 - (1 - sim ** r) ** t
+
     return p_coll
 
 def collision_prob_alpha(sim, r, t, alpha=1):
@@ -184,13 +278,18 @@ def collision_prob_alpha(sim, r, t, alpha=1):
         an integer or 1D vector of collision probabilities in [0, 1]
     '''
     p_coll = 0
+
     for i in range(int(alpha), t + 1):
         p_coll += math.comb(t, i) * (sim ** r) ** i * (1 - sim ** r) ** (t - i)
+
     if alpha == 1:
-        # ensure we achieve the same s-curve as the equivalent vanilar LSH scheme when alpha=1
+
+        # Ensure we achieve the same s-curve as the equivalent vanilla LSH scheme when alpha=1
         vanilla_p_coll = collision_prob(sim, r, t)
+
         # There can be small discrepancies here
         assert np.abs(p_coll - vanilla_p_coll).max() < 1e-5
+
     return p_coll
 
 def plot_collision_prob(rs, ts):
